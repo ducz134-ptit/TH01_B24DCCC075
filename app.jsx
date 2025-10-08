cac2197f92ab481650fcc335c138ca883383ab63
Triệu Quang Đức/B24DCCC075-JS04 <ductq.ptit@gmail.com>
2025-10-08 09:38:53 +0700

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import CountryList from './components/CountryList';
+import CountryDetail from './components/CountryDetail';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<CountryList />} />
+      <Route path="/country/:name" element={<CountryDetail />} />
+    </Routes>
+  );
+}
+
+export default App;
