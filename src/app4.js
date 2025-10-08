5110853570fc9151cbf16925b337b79cfb91330d
Triệu Quang Đức/B24DCCC075-JS04 <ductq.ptit@gmail.com>
2025-10-08 10:09:33 +0700

    Add files via upload

--- src/app4.js
+++ src/app4.js
@@ -1,54 +1,31 @@
 import React, { useState } from "react";
+function ColorBox(props) {
+  const styleBox = {
+    width: "200px",
+    height: "200px",
+    margin: "20px auto",
+    backgroundColor: props.color,
+    border: "2px solid black"
+  };
 
-function StudentCard(props) {
-  const [showDetail, setShowDetail] = useState(false);
+  return <div style={styleBox}></div>;
+}
 
-  function toggleDetail() {
-    setShowDetail(!showDetail);
+export default function ColorApp() {
+  const colors = ["red", "green", "blue", "yellow"];
+  const [color, setColor] = useState("red"); 
+  function changeColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    setColor(colors[randomIndex]);
   }
 
   return (
-    <div style={{
-      border: "1px solid black",
-      borderRadius: "8px",
-      padding: "10px",
-      margin: "10px auto",
-      width: "250px",
-      textAlign: "center"
-    }}>
-      <h3>{props.name}</h3>
-      <button onClick={toggleDetail}>
-        {showDetail ? "Ẩn chi tiết" : "Xem chi tiết"}
-      </button>
-
-      {showDetail && (
-        <div style={{ marginTop: "10px" }}>
-          <p>Tuổi: {props.age}</p>
-          <p>Lớp: {props.className}</p>
-        </div>
-      )}
-    </div>
-  );
-}
-export default function StudentApp() {
-  const students = [
-    { name: "Nguyễn Văn A", age: 20, className: "CNTT1" },
-    { name: "Trần Thị B", age: 21, className: "CNTT2" },
-    { name: "Lê Văn C", age: 19, className: "CNTT3" },
-  ];
-
-  return (
-    <div style={{ textAlign: "center", marginTop: "30px" }}>
-      <h1>Danh sách sinh viên</h1>
-
-      {students.map((stu, index) => (
-        <StudentCard
-          key={index}
-          name={stu.name}
-          age={stu.age}
-          className={stu.className}
-        />
-      ))}
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Ứng dụng Đổi màu nền</h1>
+      
+ 
+      <ColorBox color={color} />
+      <button onClick={changeColor}>Đổi màu</button>
     </div>
   );
 }
